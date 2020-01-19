59c126401581eda3a8c2088d3e9c4bb50593f2b3
yonyalva <alexjalv01@yahoo.com>
2020-01-18 23:03:26 -0500

    First commit

---
+++ precache-manifest.4462154d59e9e3553ee0bbc6a0c92beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d6b0114788325201419",
+    "url": "/casting-agency/static/css/main.1a0ccaae.chunk.css"
+  },
+  {
+    "revision": "0d6b0114788325201419",
+    "url": "/casting-agency/static/js/main.0d6b0114.chunk.js"
+  },
+  {
+    "revision": "20b3a0fc4ca9ae391600",
+    "url": "/casting-agency/static/js/1.20b3a0fc.chunk.js"
+  },
+  {
+    "revision": "3a0303e881df96e5a05b",
+    "url": "/casting-agency/static/js/runtime~main.3a0303e8.js"
+  },
+  {
+    "revision": "29912dcdbda76691cde46786c1e5679c",
+    "url": "/casting-agency/index.html"
+  }
+];
